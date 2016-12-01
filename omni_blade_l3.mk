064e40c2437e8d3e5788eb4eefbc138313720521
Grace5921 <androidlover5842@gmail.com>
2016-12-01 21:02:56 +0530

    initial commit

---
+++ omni_blade_l3.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/zte/blade_l3/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := blade_l3
+PRODUCT_NAME := omni_blade_l3
+PRODUCT_BRAND := ZTE
+PRODUCT_MANUFACTURER := ZTE
+PRODUCT_MODEL := BLADE L3
